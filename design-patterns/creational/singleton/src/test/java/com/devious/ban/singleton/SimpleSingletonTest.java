9ea9f5715965dc4664f8b22bc7cc3a57c215ee24
Ban Finch <rheriel@hotmail.co.jp>
2021-10-24 14:24:52 -0500

    Add test to see if Singleton is thread safe

--- design-patterns/creational/singleton/src/test/java/com/devious/ban/singleton/SimpleSingletonTest.java
+++ design-patterns/creational/singleton/src/test/java/com/devious/ban/singleton/SimpleSingletonTest.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class SimpleSingletonTest {
@@ -16,5 +23,23 @@ public class SimpleSingletonTest {
         for (int i = 0; i < 1000; i++) {
             assertEquals(singleton, SimpleSingleton.getInstance());
         }
+    }
+
+    @Test
+    void shouldFailThreadSafetyCheck() throws InterruptedException {
+        final ExecutorService executor = Executors.newFixedThreadPool(10);
+        final Set<SimpleSingleton> shouldOnlyHaveOneElement = new HashSet<>();
+
+        for (int i = 0; i < 10000; i++) {
+            executor.execute(() -> {
+                final SimpleSingleton singleton = SimpleSingleton.getInstance();
+                shouldOnlyHaveOneElement.add(singleton);
+            });
+        }
+
+        assertNotEquals(1, shouldOnlyHaveOneElement.size());
+
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.MINUTES);
     }
 }
